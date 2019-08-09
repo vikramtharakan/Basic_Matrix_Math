b176b53e0a8e8ca3191e16bbcbc2d1ad28739119
Vikram Tharakan <44689689+vikramtharakan@users.noreply.github.com>
2019-08-08 17:01:50 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+from setuptools import setup
+
+setup(name='basic_matrix_algebra',
+      version='0.1',
+      description='Performs basic matrix algebra',
+      packages=['basic_matrix_algebra'],
+      zip_safe=False)
